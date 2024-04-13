9c364b5579961080ff9cc292e4c63df09bc23189
Connor Clarke <connorclarke1@yahoo.com>
2024-04-13 14:42:50 +0100

    Added Nutrition Card

---
+++ resources/views/components/nutrition-card.blade.php
@@ -0,0 +1,21 @@
+<div class="nutrition-card">
+    <h2 class="nutrition-card-title">Nutritional Information</h2>
+    <table class="nutrition-table">
+        <tr>
+            <th>Calories</th>
+            <td>{{ $calories }}</td>
+        </tr>
+        <tr>
+            <th>Protein</th>
+            <td>{{ $protein }}</td>
+        </tr>
+        <tr>
+            <th>Carbs</th>
+            <td>{{ $carbs }}</td>
+        </tr>
+        <tr>
+            <th>Fat</th>
+            <td>{{ $fat }}</td>
+        </tr>
+    </table>
+</div>

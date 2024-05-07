70e9ee1d6c29aa9fad08e2cc0bc4ec0022c6e042
Connor Clarke <connorclarke1@yahoo.com>
2024-05-07 11:50:16 +0100

    Barcode scanning now also displays photo retrieved from internet

--- resources/views/barcode-product-creation.blade.php
+++ resources/views/barcode-product-creation.blade.php
@@ -4,6 +4,9 @@
         <div class="w-1/2 mr-4">
             <x-barcode-product-creation-form :productData="$productData" />
         </div>
+        <div class="w-1/2">
+            <img src="{{$productData['image_url']}}" alt="Retrieved Photo">
+        </div>
     </div>
  
 </x-app-layout>
